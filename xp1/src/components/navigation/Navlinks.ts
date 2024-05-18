35e2f96d2bf6d77d8487a1e293f5f32abc04e496
AlfMeodel <alf.meodel@gmail.com>
2024-05-18 11:57:07 +0200

    creation de ma premiere app d'entrainement

---
+++ xp1/src/components/navigation/Navlinks.ts
@@ -0,0 +1,25 @@
+export interface Routes {
+    path: string,
+    name: string
+}
+
+export let routes: Routes[] = [
+    {
+        path: "/",
+        name: "Home"
+    },
+    {
+        path: "/page0",
+        name: "Page0"
+    },
+    {
+        path: "/page1",
+        name: "Page1"
+    },
+    {
+        path: "/page2",
+        name: "Page2"
+    },
+
+
+]
