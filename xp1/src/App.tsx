66485238eded5ca7de3de2979a07a99d832312f6
AlfMeodel <alf.meodel@gmail.com>
2024-05-25 13:52:05 +0200

    resolution de mon premier conflit git ^^ yoyhouuuw

--- xp1/src/App.tsx
+++ xp1/src/App.tsx
@@ -3,17 +3,11 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Navigation from './components/navigation/Navigation';
-<<<<<<< HEAD
-import { useSelector } from 'react-redux';
-import { ChargementInterface } from './components/redux/chargementSlice';
-import Spiner from './components/redux/Spiner';
-=======
 import Inscription from './pages/Inscription';
 import Connexion from './pages/Connexion';
 import { useSelector } from 'react-redux';
 import { ChargementInterface } from './redux/chargementSlice';
 import Spiner from './redux/Spiner';
->>>>>>> 34667019b97ad9fcd72b12038480b4b4582ab1e5
 
 function App() {
   let { chargement } = useSelector((state: { chargementStore: ChargementInterface }) => state.chargementStore)
--- xp1/src/index.tsx
+++ xp1/src/index.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import { store } from './components/redux/store';
-import { Provider } from 'react-redux';
-=======
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
->>>>>>> 34667019b97ad9fcd72b12038480b4b4582ab1e5
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
